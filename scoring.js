195785349d9f0f4e216aa748583505291d347574
Stephen <stevemuller@gmail.com>
2016-08-18 15:05:42 -0700

    functional beta

--- scoring.js
+++ scoring.js
@@ -20,27 +20,40 @@ function distance(lat1, lon1, lat2, lon2) {
 }
 
 /**
-* Add points for taking hint
+*
 */
-
-// for score management
-
-var scoreBoard = {
-  points: 0,
-  addPoints: function(amount) {
-    this.points += amount;
+function ScoreBoard(starting) {
+  this.score = starting;
+  this.getScore = function() {
+    return this.score();
+  };
+  // adding the points for the distance between guess and coords or if use a hint
+  this.addPoints = function(amount) {
+    this.score += Math.ceil(amount);
+  };
+  // removes points if they guess a hint correctly
+  this.removePoints = function(amount) {
+    this.score -= amount;
   }
-};
+}
+
+
 
 /**
-* score a choice
+* take in a list of coordinates and return the distance
 */
-function scoreAttempt(coordinates, scoreBoard) {
-  scoreBoard.addPoints(function(coordinates) {
-    var lat1 = coordinates[0];
-    var lon1 = coordinates[1];
-    var lat2 = coordinates[2];
-    var lon2 = coordinates[3];
-    return distance(lat1, lon1, lat2, lon2)
-  })
+function milesBetweenPoints(coordinates) {
+  var lat1 = coordinates[0];
+  var lon1 = coordinates[1];
+  var lat2 = coordinates[2];
+  var lon2 = coordinates[3];
+  return distance(lat1, lon1, lat2, lon2)
 }
+
+
+
+//test stuff
+var testScoreboard = new ScoreBoard(0);
+var testCoords = [45.520899, -122.683658, 45.522989, -122.687199];
+testScoreboard.addPoints(milesBetweenPoints(testCoords));
+console.log(testScoreboard);
