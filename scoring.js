86e96ac1adbfc84af054844966eb2881d56b57c4
Stephen <stevemuller@gmail.com>
2016-08-18 11:44:59 -0700

    writing up notes

---
+++ scoring.js
@@ -0,0 +1,6 @@
+//Scoring Module
+
+// The lower score the better
+// Points are given for each mile between the coordinate selected and the coordinate from the flickr image
+// you can also get points for taking the hint
+//
