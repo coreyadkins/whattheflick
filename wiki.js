c950c5b8baa777d026b1aedacd5ef6c8f51bf823
Stephen <stevemuller@gmail.com>
2016-08-22 12:24:37 -0700

    now expects country name

--- wiki.js
+++ wiki.js
@@ -1,7 +1,8 @@
+'use strict';
 //Module for getting 'clues' from wikipedia, EG flags or language
 
-// test stuff
-var country = {country: 'Germany'};
+// // test stuff
+// var country = {country: 'Germany'};
 
 
 
@@ -9,25 +10,25 @@ var country = {country: 'Germany'};
 * Uses the object from the flickr request to get information from wikipedia to
 * generate a request for the wikipedia information. Returns a promise.
 */
-function getCountryClues(flickrCountry) {
+function getCountryFlag(country) {
   var PARAMS = {
-    'action': 'query', 'titles': flickrCountry['country'], format: 'json',
+    'action': 'query', 'titles': country, format: 'json',
     prop: 'pageimages'
   };
-  var url = 'https://en.wikipedia.org/w/api.php'
+  var url = 'https://en.wikipedia.org/w/api.php';
   return Promise.resolve($.ajax({
     dataType: 'jsonp',
     url: url,
     data: PARAMS
-    }));
+  }));
 }
 
 /**
 * sends the request out for things
 */
-var urlOutput = getCountryClues(country).then(function(fromJSON) {
-  console.log(fromJSON)
-  return makeThumbnailGreatAgain(stripUrlFromJson(fromJSON))
+var urlOutput = getCountryFlag(country).then(function(fromJSON) {
+  // console.log(fromJSON);
+  return create500PxThumb(stripUrlFromJson(fromJSON));
 });
 
 
@@ -37,20 +38,20 @@ var urlOutput = getCountryClues(country).then(function(fromJSON) {
 */
 function stripUrlFromJson(jsonObj) {
   var siteIDArray = Object.keys(jsonObj.query.pages);
-  console.log(siteIDArray);
+  // console.log(siteIDArray);
   var siteID = siteIDArray[0];
-  console.log(siteID)
+  // console.log(siteID);
   var output = jsonObj.query.pages[siteID].thumbnail.source;
-  console.log(output);
-  return output
+  // console.log(output);
+  return output;
 }
 
 /**
 * munges URL to request a bigger thumbnail, makes wikipedia pay for it
 */
-function makeThumbnailGreatAgain(url) {
+function create500PxThumb(url) {
   var re = /\/\d*[p][x]/i;
   var output = url.replace(re, '/500px');
-  console.log(output);
-  return output
+  // console.log(output);
+  return output;
 }
